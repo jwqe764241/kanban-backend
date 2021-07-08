afc1564e6116743698f448c2a329a8fb09c8f2ae
jwqe764241 <jwqe764241@gmail.com>
2021-07-08 22:47:30 +0900

    Init project

---
+++ src/main/java/com/standardkim/kanban/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.standardkim.kanban;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KanbanApplication.class);
+	}
+
+}
