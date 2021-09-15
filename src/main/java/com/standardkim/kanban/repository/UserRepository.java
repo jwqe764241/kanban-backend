0912ae47054e7c20a4e4c40fa526b9176fdfafc2
jwqe764241 <jwqe764241@gmail.com>
2021-09-15 21:33:02 +0900

    Update user suggestion query to exclude invited member

--- src/main/java/com/standardkim/kanban/repository/UserRepository.java
+++ src/main/java/com/standardkim/kanban/repository/UserRepository.java
@@ -12,6 +12,9 @@ public interface UserRepository extends CrudRepository<User, Long> {
 	Optional<User> findByLogin(String login);
 	boolean existsByLogin(String login);
 
-	@Query(value = "select * from user u where u.login like ?2% and not exists (select 1 from project_member m where m.project_id = ?1 AND m.user_id = u.id limit 1) limit 10", nativeQuery = true)
+	@Query(value = "select * from user u where u.login like ?2% " + 
+		"and not exists (select 1 from project_member m where m.project_id = ?1 and m.user_id = u.id limit 1) " + 
+		"and not exists (select 1 from project_invitation i where i.project_id = ?1 and i.invited_user_id = u.id limit 1) " +
+		"limit 10", nativeQuery = true)
 	List<User> findUserSuggestions(Long id, String query);
 }
