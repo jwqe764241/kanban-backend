37171af41ca0c6e5f6795b187a8916ed21b30bd3
jwqe764241 <jwqe764241@gmail.com>
2021-09-16 13:53:45 +0900

    Update message text to mime message

--- src/main/java/com/standardkim/kanban/service/MailService.java
+++ src/main/java/com/standardkim/kanban/service/MailService.java
@@ -1,12 +1,21 @@
 package com.standardkim.kanban.service;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.mail.internet.MimeMessage;
+
 import com.standardkim.kanban.dto.MailDto.ProjectInvitationMailInfo;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -14,31 +23,47 @@ import lombok.RequiredArgsConstructor;
 public class MailService {
 	private final JavaMailSender mailSender;
 
+	private final Configuration configuration;
+
 	@Value("${config.mail.fromAddress}")
 	private String fromAddress;
 
 	@Value("${config.invitation.acceptInvitationUrl}")
 	private String acceptInvitationUrl;
 
-	public void send(SimpleMailMessage message) {
-        message.setFrom(fromAddress);
+	public String getProjectInvitationMessage(ProjectInvitationMailInfo info) throws IOException, TemplateException {
+		Map<String, Object> model = new HashMap<>();
+		model.put("inviterLogin", info.getInviterLogin());
+		model.put("projectName", info.getProjectName());
+		model.put("url", String.format(acceptInvitationUrl, info.getProjectId()));
 
-		mailSender.send(message);
+		StringWriter writer = new StringWriter();
+		configuration.getTemplate("mail/test.ftlh").process(model, writer);
+
+		return writer.getBuffer().toString();
 	}
 
 	public void sendProjectInvitationMessage(ProjectInvitationMailInfo info) {
 		String title = String.format("[Kanban] @%s has invited you to join the \"%s\" project", 
 			info.getInviterLogin(), info.getProjectName());
 
-		StringBuilder builder = new StringBuilder();
-			builder.append(String.format("@%s has invited you to join the \"%s\" project\n", 
-				info.getInviterLogin(), info.getProjectName()));
-			builder.append(String.format(acceptInvitationUrl, info.getProjectId()));
+		String text;
+		try {
+			text = getProjectInvitationMessage(info);
+		} catch (Exception e) {
+			text = "";
+		}
 
-		SimpleMailMessage message = new SimpleMailMessage();
-			message.setTo(info.getInviteeMailAddress());
-			message.setSubject(title);
-			message.setText(builder.toString());
-		send(message);
+		try {
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message);
+				helper.setSubject(title);
+				helper.setTo(info.getInviteeMailAddress());
+				helper.setText(text, true);
+			mailSender.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ;
+		}
 	}
 }
