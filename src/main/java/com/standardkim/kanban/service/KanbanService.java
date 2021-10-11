71c295f2ad1d59e80c4f2798b38cec89f50e6068
jwqe764241 <jwqe764241@gmail.com>
2021-10-11 20:01:47 +0900

    Rename KanbanService methods

--- src/main/java/com/standardkim/kanban/service/KanbanService.java
+++ src/main/java/com/standardkim/kanban/service/KanbanService.java
@@ -31,28 +31,28 @@ public class KanbanService {
 	private final ModelMapper modelMapper;
 
 	@Transactional(readOnly = true)
-	public List<KanbanDetail> getKanbanDetailsByProjectId(Long projectId) {
+	public List<KanbanDetail> findKanbanDetailByProjectId(Long projectId) {
 		List<KanbanSequence> kanbanSequences = kanbanSequenceService.findByProjectIdAndNotDeleted(projectId);
 		List<KanbanDetail> kanbanDetails = modelMapper.map(kanbanSequences, new TypeToken<List<KanbanDetail>>(){}.getType());
 		return kanbanDetails;
 	}
 
 	@Transactional(readOnly = true)
-	public KanbanDetail getKanbanDetailBySequenceId(Long projectId, Long sequenceId) {
+	public KanbanDetail findKanbanDetailByProjectIdAndSequenceId(Long projectId, Long sequenceId) {
 		KanbanSequence kanbanSequence = kanbanSequenceService.findByProjectIdAndSequenceId(projectId, sequenceId);
 		KanbanDetail kanbanDetail = modelMapper.map(kanbanSequence, KanbanDetail.class);
 		return kanbanDetail;
 	}
 
 	@Transactional(readOnly = true)
-	public Kanban getKanbanBySequenceId(Long projectId, Long sequenceId) {
+	public Kanban findKanbanByProjectIdAndSequenceId(Long projectId, Long sequenceId) {
 		KanbanSequence kanbanSequence = kanbanSequenceService.findByProjectIdAndSequenceId(projectId, sequenceId);
 		Optional<Kanban> kanban = kanbanRepository.findById(kanbanSequence.getId());
 		return kanban.orElseThrow(() -> new ResourceNotFoundException("resource not found"));
 	}
 
 	@Transactional(rollbackFor = Exception.class)
-	public KanbanDetail createKanban(Long projectId, CreateKanbanParam createKanbanParam) {
+	public KanbanDetail create(Long projectId, CreateKanbanParam createKanbanParam) {
 		Project project = projectService.findById(projectId);
 		Kanban kanban = Kanban.builder()
 			.name(createKanbanParam.getName())
@@ -67,15 +67,15 @@ public class KanbanService {
 	}
 
 	@Transactional(rollbackFor = Exception.class)
-	public void updateKanban(Long projectId, Long sequenceId, UpdateKanbanParam updateKanbanParam) {
-		Kanban kanban = getKanbanBySequenceId(projectId, sequenceId);
+	public void update(Long projectId, Long sequenceId, UpdateKanbanParam updateKanbanParam) {
+		Kanban kanban = findKanbanByProjectIdAndSequenceId(projectId, sequenceId);
 		kanban.updateKanban(updateKanbanParam);
 	}
 
 	@Transactional(rollbackFor = Exception.class)
-	public void deleteKanban(Long projectId, Long sequenceId) {
+	public void delete(Long projectId, Long sequenceId) {
 		try {
-			Kanban kanban = getKanbanBySequenceId(projectId, sequenceId);
+			Kanban kanban = findKanbanByProjectIdAndSequenceId(projectId, sequenceId);
 			kanban.updateToDeleted();
 		}
 		catch (ResourceNotFoundException e) {
