3954f2734391540d8bb5e35cb2d35a05c04cb698
jwqe764241 <jwqe764241@gmail.com>
2021-11-24 13:25:16 +0900

    Fix constraint error when delete taskcolumn that has tasks

--- src/main/java/com/standardkim/kanban/repository/TaskRepository.java
+++ src/main/java/com/standardkim/kanban/repository/TaskRepository.java
@@ -5,6 +5,9 @@ import java.util.List;
 import com.standardkim.kanban.entity.Task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 	List<Task> findByTaskColumnId(Long taskColumnId);
@@ -12,4 +15,14 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 	Task findByIdAndTaskColumnId(Long id, Long taskColumnId);
 
 	Task findByPrevIdAndTaskColumnId(Long prevId, Long taskColumnId);
+
+	@Transactional
+	@Modifying
+	@Query("delete from Task t where t.taskColumnId = ?1")
+	void deleteByTaskColumnId(Long taskColumnId);
+
+	@Transactional
+	@Modifying
+	@Query("update Task t set t.prevId = null where t.taskColumnId = ?1")
+	void updatePrevIdToNullByTaskColumnId(Long taskColumnId);
 }
