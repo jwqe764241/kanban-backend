307e4caf0eb919473d611eb743710891c7672753
jwqe764241 <jwqe764241@gmail.com>
2021-12-07 14:01:38 +0900

    Move methods finding KanbanSequence to KanbanSequenceService

--- src/main/java/com/standardkim/kanban/service/KanbanService.java
+++ src/main/java/com/standardkim/kanban/service/KanbanService.java
@@ -1,15 +1,11 @@
 package com.standardkim.kanban.service;
 
-import java.util.List;
-
 import com.standardkim.kanban.dto.KanbanDto.CreateKanbanParam;
 import com.standardkim.kanban.dto.KanbanDto.UpdateKanbanParam;
 import com.standardkim.kanban.entity.Kanban;
-import com.standardkim.kanban.entity.KanbanSequence;
 import com.standardkim.kanban.entity.Project;
 import com.standardkim.kanban.exception.kanban.KanbanNotFoundException;
 import com.standardkim.kanban.repository.KanbanRepository;
-import com.standardkim.kanban.repository.KanbanSequenceRepository;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,24 +17,11 @@ import lombok.RequiredArgsConstructor;
 public class KanbanService {
 	private final KanbanRepository kanbanRepository;
 
-	private final KanbanSequenceRepository kanbanSequenceRepository;
-
 	private final ProjectService projectService;
 
 	@Transactional(readOnly = true)
-	public List<KanbanSequence> findByProjectId(Long projectId) {
-		return kanbanSequenceRepository.findByProjectIdAndIsDeletedOrderBySequenceId(projectId, false);
-	}
-
-	@Transactional(readOnly = true)
-	public KanbanSequence findByProjectIdAndSequenceId(Long projectId, Long sequenceId) {
-		return kanbanSequenceRepository.findByProjectIdAndSequenceId(projectId, sequenceId)
-			.orElseThrow(() -> new KanbanNotFoundException("kanban not found"));
-	}
-
-	@Transactional(readOnly = true)
-	public KanbanSequence findById(Long kanbanId) {
-		return kanbanSequenceRepository.findById(kanbanId)
+	public Kanban findByProjectIdAndSequenceId(Long projectId, Long sequenceId) {
+		return kanbanRepository.findByProjectIdAndSequenceId(projectId, sequenceId)
 			.orElseThrow(() -> new KanbanNotFoundException("kanban not found"));
 	}
 
@@ -51,8 +34,7 @@ public class KanbanService {
 
 	@Transactional(rollbackFor = Exception.class)
 	public Kanban update(Long projectId, Long sequenceId, UpdateKanbanParam updateKanbanParam) {
-		Kanban kanban = kanbanRepository.findByProjectIdAndSequenceId(projectId, sequenceId)
-			.orElseThrow(() -> new KanbanNotFoundException("kanban not found"));
+		Kanban kanban = findByProjectIdAndSequenceId(projectId, sequenceId);
 		kanban.updateKanban(updateKanbanParam);
 		return kanban;
 	}
