e0c2062b77f444a2e08db66e933ea130f82018bd
jwqe764241 <jwqe764241@gmail.com>
2021-12-10 13:22:56 +0900

    Add BaseTimeEntity

---
+++ src/main/java/com/standardkim/kanban/entity/common/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.standardkim.kanban.entity.common;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+	@CreatedDate
+	@Column(name = "create_at", updatable = false, nullable = false)
+	private LocalDateTime createdAt;
+}
