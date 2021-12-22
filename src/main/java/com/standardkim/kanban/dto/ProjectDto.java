376c1defea2470225dd1c4f2b0571cf6b66be9de
jwqe764241 <jwqe764241@gmail.com>
2021-12-22 23:04:18 +0900

    Add update project

--- src/main/java/com/standardkim/kanban/dto/ProjectDto.java
+++ src/main/java/com/standardkim/kanban/dto/ProjectDto.java
@@ -36,4 +36,14 @@ public class ProjectDto {
 		private String registerUsername;
 		private LocalDateTime createdAt;
 	}
+
+	@Getter
+	@Builder
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@AllArgsConstructor
+	public static class UpdateProjectParam {
+		@NotBlank
+		@Size(min = 2, max = 50)
+		private String name;
+	}
 }
