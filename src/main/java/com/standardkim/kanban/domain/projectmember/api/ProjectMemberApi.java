137c65354558f1ce63c45959d506d1f3bcc3cb9a
jwqe764241 <jwqe764241@gmail.com>
2022-01-17 14:32:09 +0900

    Move getProjectMemberSuggestions to UserApi

--- src/main/java/com/standardkim/kanban/domain/projectmember/api/ProjectMemberApi.java
+++ src/main/java/com/standardkim/kanban/domain/projectmember/api/ProjectMemberApi.java
@@ -6,9 +6,6 @@ import com.standardkim.kanban.domain.auth.dto.SecurityUser;
 import com.standardkim.kanban.domain.projectmember.application.ProjectMemberService;
 import com.standardkim.kanban.domain.projectmember.domain.ProjectMember;
 import com.standardkim.kanban.domain.projectmember.dto.ProjectMemberDetail;
-import com.standardkim.kanban.domain.user.application.UserService;
-import com.standardkim.kanban.domain.user.domain.User;
-import com.standardkim.kanban.domain.user.dto.SuggestionUserDetail;
 import com.standardkim.kanban.global.util.SecurityContextFacade;
 
 import org.modelmapper.ModelMapper;
@@ -19,7 +16,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,8 +26,6 @@ import lombok.RequiredArgsConstructor;
 public class ProjectMemberApi {
 	private final ProjectMemberService projectMemberService;
 
-	private final UserService userService;
-
 	private final ModelMapper modelMapper;
 
 	@GetMapping("/projects/{projectId}/members")
@@ -41,15 +35,6 @@ public class ProjectMemberApi {
 		List<ProjectMember> projectMembers = projectMemberService.findByProjectId(projectId);
 		List<ProjectMemberDetail> projectMemberDetails = modelMapper.map(projectMembers, new TypeToken<List<ProjectMemberDetail>>(){}.getType());
 		return projectMemberDetails;
-	}
-
-	@GetMapping("/projects/{projectId}/members/suggestions")
-	@ResponseStatus(HttpStatus.OK)
-	@PreAuthorize("isProjectOwner(#projectId)")
-	public List<SuggestionUserDetail> getProjectMemberSuggestions(@PathVariable Long projectId, @RequestParam("q") String query) {
-		List<User> users = userService.findNotMemberOrNotInvitedUser(projectId, query);
-		List<SuggestionUserDetail> suggestionUserDetails = modelMapper.map(users, new TypeToken<List<SuggestionUserDetail>>(){}.getType());
-		return suggestionUserDetails;
 	}
 
 	@DeleteMapping("/projects/{projectId}/members/{userId}")
