c58eb50422952077fbc2850d062079a143e15294
jwqe764241 <jwqe764241@gmail.com>
2022-01-25 21:37:26 +0900

    Add `deleteByProjectId` method to ProjectMemberDeleteService

--- src/main/java/com/standardkim/kanban/domain/projectmember/dao/ProjectMemberRepository.java
+++ src/main/java/com/standardkim/kanban/domain/projectmember/dao/ProjectMemberRepository.java
@@ -5,8 +5,16 @@ import java.util.List;
 import com.standardkim.kanban.domain.projectmember.domain.ProjectMember;
 import com.standardkim.kanban.domain.projectmember.domain.ProjectMemberKey;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface ProjectMemberRepository extends CrudRepository<ProjectMember, ProjectMemberKey>{
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, ProjectMemberKey>{
 	List<ProjectMember> findByProjectIdOrderByCreatedAtAsc(Long projectId);
+
+	@Transactional
+	@Modifying
+	@Query("delete from ProjectMember pm where pm.id.projectId = ?1")
+	void deleteByProjectId(Long projectId);
 }
