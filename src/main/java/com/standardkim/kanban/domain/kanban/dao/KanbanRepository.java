92f9527fdf49007d247337d64984a79b092edbbd
jwqe764241 <jwqe764241@gmail.com>
2022-01-25 21:33:49 +0900

    Add `deleteByProjectId` method to KanbanDeleteService

--- src/main/java/com/standardkim/kanban/domain/kanban/dao/KanbanRepository.java
+++ src/main/java/com/standardkim/kanban/domain/kanban/dao/KanbanRepository.java
@@ -1,5 +1,6 @@
 package com.standardkim.kanban.domain.kanban.dao;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.standardkim.kanban.domain.kanban.domain.Kanban;
@@ -10,4 +11,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface KanbanRepository extends CrudRepository<Kanban, Long> {
 	@Query(value = "SELECT k.* FROM kanban k LEFT JOIN v_kanban_sequence ks ON ks.project_id = ?1 and ks.sequence_id = ?2 WHERE k.id = ks.id", nativeQuery = true)
 	Optional<Kanban> findByProjectIdAndSequenceId(Long projectId, Long sequenceId);
+
+	@Query("select id from Kanban k where k.project.id = ?1")
+	List<Long> findIdByProjectId(Long projectId);
 }
