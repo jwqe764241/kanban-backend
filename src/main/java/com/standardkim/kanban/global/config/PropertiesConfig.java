8100b1ce42dcd1a664b6d6595b8499762d2a86d2
jwqe764241 <jwqe764241@gmail.com>
2022-02-03 16:54:43 +0900

    Add PropertiesConfig

---
+++ src/main/java/com/standardkim/kanban/global/config/PropertiesConfig.java
@@ -0,0 +1,9 @@
+package com.standardkim.kanban.global.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(value = {})
+public class PropertiesConfig {
+}
