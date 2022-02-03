8efec9a60681f9d987618cea83bbdc4095d5c270
jwqe764241 <jwqe764241@gmail.com>
2022-02-03 18:59:39 +0900

    Add ConfigProperties

--- src/main/java/com/standardkim/kanban/global/config/PropertiesConfig.java
+++ src/main/java/com/standardkim/kanban/global/config/PropertiesConfig.java
@@ -1,6 +1,7 @@
 package com.standardkim.kanban.global.config;
 
 import com.standardkim.kanban.global.config.property.AuthenticationProperties;
+import com.standardkim.kanban.global.config.property.ConfigProperties;
 import com.standardkim.kanban.global.config.property.InvitationProperties;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,6 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(value = 
-	{InvitationProperties.class, AuthenticationProperties.class})
+	{InvitationProperties.class, AuthenticationProperties.class, ConfigProperties.class})
 public class PropertiesConfig {
 }
