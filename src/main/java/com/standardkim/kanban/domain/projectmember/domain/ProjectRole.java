7162365bc4866fec5f0911e188c3f26ac29f9d3f
jwqe764241 <jwqe764241@gmail.com>
2022-02-04 16:50:52 +0900

    Add ProjectRole

---
+++ src/main/java/com/standardkim/kanban/domain/projectmember/domain/ProjectRole.java
@@ -0,0 +1,31 @@
+package com.standardkim.kanban.domain.projectmember.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"name"})})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ProjectRole {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Long id;
+
+	@Column(length = 10, updatable = false, nullable = false)
+	private String name;
+}
